7f542308b208a4cd15008caff5cc29b97563c6d5
Greg P <gorgarp@gmail.com>
2023-01-19 13:07:09 -0600

    Update main.py

--- main.py
+++ main.py
@@ -1,10 +1,10 @@
-from flask import Flask, render_template, request
 import requests
 import json
 import pickle
 from sklearn.cluster import KMeans
 import numpy as np
 import time
+from flask import Flask, render_template, request
 
 app = Flask(__name__)
 
@@ -28,41 +28,46 @@ def get_watch_history_of_all_users():
     }
     watch_history_response = requests.get(watch_history_url, headers=headers)
     watch_history_data = json.loads(watch_history_response.text)
-    watch_history = []
+    watch_history_data = []
     for session in watch_history_data["MediaContainer"]["Metadata"]:
-        watch_history.append([session["title"], session["year"], session["rating"]])
-    return watch_history
+        watch_history_data.append([session["title"], session["year"], session["rating"]])
+    return watch_history_data
+
+def make_recommendations(username):
+    # Fetch the watch history of the user
+    user_watch_history = []
+    for session in watch_history_data:
+        if session["username"] == username:
+            user_watch_history.append([session["title"], session["year"], session["rating"]])
+    # Predict the cluster of the user's watch history
+    cluster_id = kmeans.predict(user_watch_history)
+    # Make recommendations based on the cluster centroid
+    suggestions = []
+    for title, year, rating in kmeans.cluster_centers_[cluster_id]:
+        suggestions.append(f"{title} ({year})")
+    return suggestions
 
 @app.route('/', methods=['GET', 'POST'])
 def index():
     if request.method == 'POST':
-        #get the watch history of all users
+        # Get the watch history of all users
         watch_history_data = get_watch_history_of_all_users()
-
-        # Load or train the clustering model
+        try:
+            # Load the clustering model
+            kmeans = load_clustering_model()
+        except FileNotFoundError:
+            # Train the clustering model
+            kmeans = train_clustering_model(watch_history_data)
+        # Get the username
+        username = request.form['username']
+        # Make recommendations
+        recommendations = make_recommend
+if __name__ == '__main__':
+    while True:
+        watch_history_data = get_watch_history_of_all_users()
         try:
             kmeans = load_clustering_model()
         except FileNotFoundError:
             kmeans = train_clustering_model(watch_history_data)
-        
-        # Predict the cluster of the current user
-        username = request.form['username']
-        user_watch_history = []
-        for session in watch_history_data:
-            if session["username"] == username:
-                user_watch_history.append([session["title"], session["year"], session["rating"]])
-        user_watch_history = np.array(user_watch_history)
-        cluster_id = kmeans.predict(user_watch_history)
-        
-        # Make suggestions
-        suggestions = []
-        for title, year, rating in kmeans.cluster_centers_[cluster_id]:
-            suggestions.append(f"{title} ({year})")
-
-        return render_template('index.html', suggestions=suggestions)
-    else:
-        return render_template('index.html')
-
-if __name__ == '__main__':
-    while True:
-        watch_history_data = get_watch_history
+        app.run(debug=True)
+        time.sleep(3600) # Pause for 1 hour (3600 seconds) before updating the watch history data again
