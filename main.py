9282d247b749376c432255993e5eaff48bc02fe3
Greg P <gorgarp@gmail.com>
2023-01-20 13:01:59 -0600

    Update main.py

--- main.py
+++ main.py
@@ -1,75 +1,78 @@
-import requests
-import json
-import pickle
-from sklearn.cluster import KMeans
+import plexapi
+from sklearn.metrics.pairwise import cosine_similarity
 import numpy as np
-import time
-from flask import Flask, render_template, request
+import pickle
 
-app = Flask(__name__)
+# Prompt the user for their Plex credentials
+username = input("Enter your Plex username: ")
+password = input("Enter your Plex password: ")
+plex = plexapi.PlexServer(PLEX_URL, username=username, password=password)
 
-def train_clustering_model(watch_history_data):
-    watch_history_data = np.array(watch_history_data)
-    kmeans = KMeans(n_clusters=5)
-    kmeans.fit(watch_history_data)
-    with open('kmeans_model.pkl', 'wb') as file:
-        pickle.dump(kmeans, file)
-    return kmeans
+try:
+    # Load the data from a file
+    with open("watch_history.pkl", "rb") as f:
+        watch_history = pickle.load(f)
+    with open("user_groups.pkl", "rb") as f:
+        user_groups = pickle.load(f)
+    with open("recommended_items.pkl", "rb") as f:
+        recommended_items = pickle.load(f)
+except:
+    # Extract the show/movie metadata and user watch history from the data
+    watch_history = plex.history()
+    metadata = [h.metadata for h in watch_history]
+    users = [h.user for h in watch_history]
+    user_history = {user: [m.ratingKey for m in metadata if m.user == user] for user in set(users)}
+    recommended_items = {}
+    
+    # Use KMeans clustering to group users based on their watch history
+    model = KMeans(n_clusters=5)
+    user_groups = model.fit_predict(user_history)
 
-def load_clustering_model():
-    with open('kmeans_model.pkl', 'rb') as file:
-        kmeans = pickle.load(file)
-    return kmeans
+# Ask the user if they are trying to find a show or movie
+media_type = input("Are you trying to find a show or movie? ")
 
-def get_watch_history_of_all_users():
-    watch_history_url = f"https://plex.tv/api/v2/users/accounts/sessions"
-    headers = {
-        "X-Plex-Token": "YOUR_PLEX_API_TOKEN"
-    }
-    watch_history_response = requests.get(watch_history_url, headers=headers)
-    print(watch_history_response.text)
-    watch_history_data = json.loads(watch_history_response.text)
-    print(watch_history_response.status_code)
-    watch_history_data = []
-    for session in watch_history_data["MediaContainer"]["Metadata"]:
-        watch_history_data.append([session["title"], session["year"], session["rating"]])
-    return watch_history_data
+# Analyze the metadata of shows/movies in each group to make recommendations
+for group in set(user_groups):
+    group_metadata = [m for i, m in enumerate(metadata) if user_groups[i] == group and m.type == media_type]
+    group_users = [user for i, user in enumerate(users) if user_groups[i] == group]
 
-def make_recommendations(username):
-    # Fetch the watch history of the user
-    user_watch_history = []
-    for session in watch_history_data:
-        if session["username"] == username:
-            user_watch_history.append([session["title"], session["year"], session["rating"]])
-    # Predict the cluster of the user's watch history
-    cluster_id = kmeans.predict(user_watch_history)
-    # Make recommendations based on the cluster centroid
-    suggestions = []
-    for title, year, rating in kmeans.cluster_centers_[cluster_id]:
-        suggestions.append(f"{title} ({year})")
-    return suggestions
+    # Extracting features for content-based filtering
+    genres = [m.genres for m in group_metadata]
+    actors = [m.actors for m in group_metadata]
+    directors = [m.directors for m in group_metadata]
+popularity = [m.popularity for m in group_metadata]
+    ratings = [m.rating for m in group_metadata]
+    releasedates = [m.releasedate for m in group_metadata]
+    tags = [m.tags for m in group_metadata]
+    year = [m.year for m in group_metadata]
+    features = np.array([genres, actors, directors, popularity, ratings, releasedates, tags, year])
 
-@app.route('/', methods=['GET', 'POST'])
-def index():
-    if request.method == 'POST':
-        # Get the watch history of all users
-        watch_history_data = get_watch_history_of_all_users()
-        try:
-            # Load the clustering model
-            kmeans = load_clustering_model()
-        except FileNotFoundError:
-            # Train the clustering model
-            kmeans = train_clustering_model(watch_history_data)
-        # Get the username
-        username = request.form['username']
-        # Make recommendations
-        recommendations = make_recommend
-if __name__ == '__main__':
-    while True:
-        watch_history_data = get_watch_history_of_all_users()
-        try:
-            kmeans = load_clustering_model()
-        except FileNotFoundError:
-            kmeans = train_clustering_model(watch_history_data)
-        app.run(debug=True)
-        time.sleep(3600) # Pause for 1 hour (3600 seconds) before updating the watch history data again
+    # Compute cosine similarity
+    cosine_sim = cosine_similarity(features)
+    content_based_recs = []
+    for i in range(len(group_metadata)):
+        similar_indices = np.argsort(cosine_sim[i])[:-11:-1]
+        similar_items = [group_metadata[i] for i in similar_indices]
+        content_based_recs.append(similar_items)
+    
+    # Make recommendations to each user
+    for i, user in enumerate(group_users):
+        recs = content_based_recs[i]
+        # Remove already watched items from recommendations
+        recs = [rec for rec in recs if rec.ratingKey not in user_history[user]]
+        # Remove items that have already been recommended
+        recs = [rec for rec in recs if rec.ratingKey not in recommended_items.keys()]
+        # Update the recommended_items dictionary
+        for rec in recs:
+            recommended_items[rec.ratingKey] = rec
+        # Print the recommendations for the user
+        print(f"Recommendations for {user}:")
+        for rec in recs:
+            print(rec.title)
+    # Save the data to a file
+    with open("watch_history.pkl", "wb") as f:
+        pickle.dump(watch_history, f)
+    with open("user_groups.pkl", "wb") as f:
+        pickle.dump(user_groups, f)
+    with open("recommended_items.pkl", "wb") as f:
+        pickle.dump(recommended_items, f)
